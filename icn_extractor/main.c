46a96ea56177d736a36fdae7a4fd2cc22723125e
3DSGuy <devguy@hotmail.com.au>
2013-11-08 00:19:30 +0800

    icn_extractor: misc licensing

--- icn_extractor/main.c
+++ icn_extractor/main.c
@@ -1,3 +1,22 @@
+/**
+Copyright 2013 3DSGuy
+
+This file is part of icn_extractor.
+
+icn_extractor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+icn_extractor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with icn_extractor.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 #include "lib.h"
 #include "bmp.h"
 #include "LodePNG/lodepng.h"
